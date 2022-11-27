dcf2a93c777e432904be38049bf3a2ed0aa7e0db
Syed Bilal Abbas <abbasb000@gmail.com>
2022-11-27 18:16:16 +0500

    Update require.sh

--- require.sh
+++ require.sh
@@ -12,6 +12,11 @@ sudo apt install rinetd
 sudo apt install gobuster
 sudo apt install 
 sudo apt update && sudo apt install -y feroxbuster
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
 
 mkdir /usr/share/OSCP
 cd /usr/share/OSCP
